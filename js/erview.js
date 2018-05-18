bf3160eee14e3ff3f1e2fceb9b0047c4dc96b89f
Shayne <shayne@emucoo.com>
2018-05-18 18:10:00 +0800

    go on

--- js/erview.js
+++ js/erview.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql')
 
 
-module.exports = function DBModel(dbinfo) {
-    this.dbinfo = {
+module.exports = function DBModel(options) {
+    this.dbinfo = options.dbinfo || {
         host: '192.168.16.173',
         user: 'emotwo',
         password: 'emu2018two',
         port: '3306',
         database: 'emucoo-cfb',
         multipleStatements: true
-    } || dbinfo;
+    }
 
     this.listeners = {
         onConnectionErr: function(err) {
@@ -17,15 +17,13 @@ module.exports = function DBModel(dbinfo) {
         },
         onSqlErr: function(err) {
             console.log(err)
-        },
-        onLoaded: function(models) {
-
         }
+    } 
+    this.listeners.onLoaded = options.onLoaded || function(models) {
+        console.log('default callback notification: onLoaded')
     }
 
     this.status = 0;
-
-
     let asyncQuery = function(conn, sql, args){
         return new Promise((resolve, reject) => {
             conn.query(sql, args, (err, rss) =>{
@@ -79,86 +77,5 @@ module.exports = function DBModel(dbinfo) {
         }
     });
 
-
-
-
-
-    this.init = function (dbinfo) {
-        var conninfo = 
-
-        
-        dbconn.connect(function (err) {
-            if (err) {
-                console.log('database connetction fail!');
-                console.log(err)
-            }
-        })
-
-        function initEntityDom(tables) {
-            var doms = []
-            tables.forEach(element => {
-                var tb = '<table class="fields">';
-                element.fields.forEach(row => {
-                    tb +='<tr><td>' + row.Field + '</td><td>' + row.Type + '</td><td>' + row.Key + '</td><tr>';
-                })
-                tb += '</table>';
-                var div = '<div id="'+ element.name +'" class="graph"><div class="title">'+ element.name +'</div><div class="content">'+ tb +'</div></div>';
-                doms.push({name: elememt.name, html: div});
-            });
-            return doms;
-        }
-
-
-        var dbtables = Array();
-        asyncQuery("show tables").then(rss => {
-            console.log('export table list')
-            rss.forEach(element => {
-                var tbl = {
-                    "name": element['Tables_in_emucoo-cfb'],
-                    "fields": []
-                }
-                dbtables.push(tbl)
-            });
-            dbtables.forEach(element => {
-                asyncQuery("desc "+ element.name).then(rss => {
-                    console.log('desc table')
-                    rss.forEach(item => {
-                        element.fields.push(item)
-                    });
-                }).catch(err => {
-                    console.log('查询表结构失败!')
-                    console.log(err)
-                });
-            });
-            console.log(dbtables);
-        }).catch(err => {
-            console.log('查询数据表列表失败了!')
-            console.log(err)
-        });
-
-
-
-
-        function loadEntity (conn) {
-            var dbtables = Array();
-            conn.query("show tables", function (err, rss) {
-                rss.forEach(element => {
-                    var tbl = {
-                        "name": element['Tables_in_emucoo-cfb'],
-                        "fields": []
-                    }
-                    conn.query('desc ' + element['Tables_in_emucoo-cfb'], function (e, rs) {
-                        rs.forEach(r => {
-                            tbl.fields.push(r)
-                        });
-                    })
-                    dbtables.push(tbl)
-                });
-            });
-            return dbtables;
-        }
-        
-
-    }
     
 }
--- js/graph.js
+++ js/graph.js
@@ -1,4 +1,6 @@
 
+(function(win) {
+
 var util = util || {};
 util.page = util.page || {};
 
@@ -16,9 +18,9 @@ util.page.getViewHeight = function () {
     return Math.max(h1, h2, h3);
 };
 
-var test = test || {};
+var Graph = Graph || {};
 
-test = {
+Graph = {
     w: 960,
     h: 480,
     styles: {
@@ -26,54 +28,49 @@ test = {
     },
     r: null, //(function(){
     connections: [], //关系集合
-    graphs: null, // rect集合
+    rects: null, // rect集合
     maps: [], //顶点和索引的对应关系，为了建立索引关系
 
     config: null
 }
 
-test.showGraph = function () {
-    var me = test,
-        w = util.page.getViewWidth(),
-        h = util.page.getViewHeight();
-
-    me.w = w - 80;
-    me.h = h - 30;
-    me.r = Raphael("canvas", me.w, me.h);
-    me.graphs = me.r.set();
-    var w = me.w,
-        h = me.h;
-
+Graph.showGraph = function () {
+    var me = Graph;
+    me.w = util.page.getViewWidth();
+    me.h = util.page.getViewHeight();
+    me.canvas = Raphael("canvas", me.w, me.h);
+    me.rects = me.canvas.set();
+    var w = me.w, h = me.h;
     var cellW = 240;
     var cellH = 120;
     var num = Math.floor(w / cellW);
-    test.renderGraph();
+    Graph.renderGraph();
 }
 
-test.renderConnections = function (lines) {
-    var me = test;
+Graph.renderConnections = function (lines) {
+    var me = Graph;
 
     $.each(lines, function (key, value) {
         var from = value.fromStateId;
         var to = value.toStateID;
-        var r = me.r;
-        var graphs = me.graphs,
+        var r = me.canvas;
+        var rects = me.rects,
             maps = me.maps;
 
-        // [maps[from]] graphs[maps[to]] 是索引值
-        me.connections.push(r.connection(graphs[maps[from]], graphs[maps[to]], '#4848fe'));
+        // [maps[from]] rects[maps[to]] 是索引值
+        me.connections.push(r.connection(rects[maps[from]], rects[maps[to]], '#4848fe'));
     });
 }
 
-test.redrawConnections = function () {
-    var me = test, r = me.r, connections = me.connections;
+Graph.redrawConnections = function () {
+    var me = Graph, r = me.canvas, connections = me.connections;
     $.each(connections, function (key, value) {
         r.connection(value);
     });
 }
 
-test.handlerMove = function (target) {
-    var me = test;
+Graph.handlerMove = function (target) {
+    var me = Graph;
 
     var x = parseInt(target.style.left, 10);
     var y = parseInt(target.style.top, 10);
@@ -89,14 +86,14 @@ test.handlerMove = function (target) {
 
     //var id = T.dom.getAttr(target,'data-id');
     var id = $(target).attr("data-id");
-    var rect = me.r.getById(id);
+    var rect = me.canvas.getById(id);
     rect.attr(att);
     me.redrawConnections();
     // me.r.safari();
 }
 
-test.createGraph = function (i, graph) {
-    var me = test;
+Graph.createGraph = function (i, graph) {
+    var me = Graph;
     var template = "";
     graph.index = i;
     // x, y 需要详看
@@ -120,7 +117,7 @@ test.createGraph = function (i, graph) {
     var temp = temp0({"data": graph});
 
     // render svg rect
-    var stateR = me.r.rect(0, 0, me.styles.point[0], me.styles.point[1]);
+    var stateR = me.canvas.rect(0, 0, me.styles.point[0], me.styles.point[1]);
 
     stateR.id = graph.id;
     stateR.attr({
@@ -147,32 +144,34 @@ test.createGraph = function (i, graph) {
         width: width
     });
 
-    me.graphs.push(stateR);
+    me.rects.push(stateR);
     me.maps[graph.id] = i;
 
     graphDom.draggable({
         cursor: "move",
         start: function () {
-            test.handlerMove(graphDom[0]);
+            Graph.handlerMove(graphDom[0]);
         },
         drag: function () {
-            test.handlerMove(graphDom[0]);
+            Graph.handlerMove(graphDom[0]);
         }
     });
 }
 
-test.renderGraph = function() {
+Graph.renderGraph = function() {
     var position_data = GraphPosition();
-    var graphsData = position_data.data.states;
+    var rectsData = position_data.data.states;
     var connectionData = position_data.data.relations;
 
-    $.each(graphsData, function(i, graph){
-        test.createGraph(i, graph);
+    $.each(rectsData, function(i, graph){
+        Graph.createGraph(i, graph);
     });
 
-    test.renderConnections(connectionData);
+    Graph.renderConnections(connectionData);
 }
 
 $(function () {
-    test.showGraph();
+    Graph.showGraph();
 });
+
+}(window));
